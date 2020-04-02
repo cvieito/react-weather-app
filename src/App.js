87ee15716738f741acdb1a4b9077c43750f62550
catesv <catesv@users.noreply.github.com>
2020-04-02 15:51:03 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <small>
-        <a href="https://github.com/cvieito/react-weather-app" target="_blank">
-          Open-source code
-        </a>{" "}
-        by Catarina Vieito.
-      </small>
+      <div className="container">
+        <h1>Weather App</h1>
+        <small>
+          <a
+            href="https://github.com/cvieito/react-weather-app"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Catarina Vieito.
+        </small>
+      </div>
     </div>
   );
 }

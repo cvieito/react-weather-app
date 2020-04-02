ca6fe1998db847c718ba1e521e64946d22133a52
catesv <catesv@users.noreply.github.com>
2020-04-02 15:23:50 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <small>
+        <a href="https://github.com/cvieito/react-weather-app" target="_blank">
+          Open-source code
+        </a>{" "}
+        by Catarina Vieito.
+      </small>
     </div>
   );
 }
